06896deda76d2df50568dd6e2330d06e7b6a8b1a
Adam Saxén <adam.saxen@gmail.com>
2017-03-24 11:17:24 +0100

    Update __init__.py

--- generic/python-entity-heater-control/heatercontrol/__init__.py
+++ generic/python-entity-heater-control/heatercontrol/__init__.py
@@ -1,7 +1,6 @@
 import ioant.proto.proto as proto
 import sys
 
-proto.python_main("../../../../../common/proto/",
-                  "generated_proto/.")
+proto.python_main("generated_proto/.")
 
 sys.path.append("generated_proto/")
