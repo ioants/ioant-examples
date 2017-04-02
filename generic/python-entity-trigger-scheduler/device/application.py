e515df95fbd6e7a75ccbe74f3fdd84573b9e6200
saxen <saxen@localhost.localdomain>
2017-04-02 19:58:37 +0200

    Improve entity trigger scheduler

--- generic/python-entity-trigger-scheduler/device/application.py
+++ generic/python-entity-trigger-scheduler/device/application.py
@@ -1,32 +1,43 @@
 # =============================================
 # Adam Saxen
 # Date: 2017-03-10
-# Description: Simple boilerplate on how to publish
-# and subscribe on messages
+# Description: Trigger scheduler
+# Schedule at what times it should trigger during the day
 # =============================================
 from ioant.sdk import IOAnt
 import logging
 import hashlib
+import schedule
+import time
 logger = logging.getLogger(__name__)
+
+
+def event_job():
+    msg = ioant.create_message("Trigger")
+    topic = ioant.get_topic_structure()
+    configuration = ioant.get_configuration()
+
+    for topic_configuration in configuration['trigger_topics']:
+        topic['global'] = topic_configuration['global']
+        topic['local'] = topic_configuration['local']
+        topic['client_id'] = topic_configuration['client_id']
+        ioant.publish(msg, topic)
+
+    return
 
 
 def setup(configuration):
     """ setup function """
     ioant.setup(configuration)
+    for t in configuration['schedule']:
+        logger.info("Scheduling time-event", {time:t})
+        schedule.every().day.at(t).do(event_job)
 
 
 def loop():
     """ Loop function """
     ioant.update_loop()
-    msg = ioant.create_message("Trigger")
-    topic = ioant.get_topic_structure()
-    configuration = ioant.get_configuration()
-    topic['global'] = configuration['trigger_topic']['global']
-    topic['local'] = configuration['trigger_topic']['local']
-    topic['client_id'] = configuration['trigger_topic']['client_id']
-
-    # publish with device topic
-    ioant.publish(msg, topic)
+    schedule.run_pending()
 
 
 def on_message(topic, message):
